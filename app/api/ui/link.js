02e5d3a8531c037eebdb68eca2ffeb0e09e11bbb
tomhannarong <hannarong.senachai@gmail.com>
2021-04-13 00:18:50 +0700

    init

---
+++ app/api/ui/link.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dashboard: '/app',
+  login: '/login',
+  market: '#',
+  email: '/app/pages/email',
+  profile: '/app/pages/user-profile',
+  calendar: '/app/pages/calendar',
+  twitter: 'https://twitter.com/ilham_meidi',
+  github: 'https://github.com/ilhammeidi',
+  pinterest: 'https://id.pinterest.com/ilhammeidi/',
+  linkedin: 'https://www.linkedin.com/in/ilhammeidi',
+  buy: 'https://themeforest.net/item/dandelion-pro-react-admin-dashboard-template/22890261',
+};
